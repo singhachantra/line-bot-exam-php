33ea37910b65fab61815ae5e55eb8a08ff3ca59f
singhachantra <50546803+singhachantra@users.noreply.github.com>
2019-05-12 20:22:18 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Gf1l0gFyV7ZL1efc45h4A20eUq8ROqBEUB0bGIvHM8OeqsMvW+b8ww4flHD0+TTUPjYRzeh8L2TMuuCtsXFZ+ZcKWIWYVZN6XetUxUrKYOA0FeK812tZOL5o+HMr6y3uYP2bkrPxK/aMgyJsRh+OTAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
